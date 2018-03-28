3180eb3335b7b2cc1b0b39f7e7ce14e4235d9a9a
John Kalinauskas <jkalinauskas@live.com>
2018-03-28 12:44:22 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,13 +1,14 @@
 
 $(document).ready(function(){
 });
-//Define a function frameIt that does not accept a parameter. The function should bind the load event that adds the class tasty to the image to add a red frame to the image.
+//Define a function frameIt that does not accept a parameer. The function should bind the LOAD event that adds the class TASTY to the image to add a red frame to the image.
 
 $('p').on ("eventname", 
 function getIt(){
   console.log("Hey");
 });
 
+$('img').on('load' ,
 function frameIt() {
   
 }
